5d92921a2d7c7787ee2baf56cb8a086ccc80b7d6
alphaStrangler <shivansh.bhr@gmail.com>
2018-05-27 17:38:00 +0530

    Added Language Support for Hindi(Language screen only)

--- app/src/main/java/xhedra/test2/MainActivity.java
+++ app/src/main/java/xhedra/test2/MainActivity.java
@@ -1,13 +1,63 @@
 package xhedra.test2;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loc_en;
+    Button loc_hi;
+    Button loc_mr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loc_mr = findViewById(R.id.mr);
+        loc_hi = findViewById(R.id.hi);
+        loc_en = findViewById(R.id.en);
+        final String loc = Locale.getDefault().toString();
+
+        loc_en.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale(loc);
+            }
+        });
+
+        loc_hi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("hi");
+            }
+        });
+
+        loc_mr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setLocale("mr");
+            }
+        });
+
+    }
+    protected void setLocale(String loc){
+        Locale myLocale = new Locale(loc);
+        Resources res = getResources();
+        DisplayMetrics myDM = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.setLocale(myLocale);
+        res.updateConfiguration(conf, myDM);
+        Intent refresh = new Intent(this, MainActivity.class);
+        startActivity(refresh);
+        finish();
+
+
     }
 }
