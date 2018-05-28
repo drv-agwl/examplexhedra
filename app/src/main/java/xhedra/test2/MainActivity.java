c82bf59c54a94f31a8f32e8c2073806f82b0a6c2
humbletechy <iit2017125@iiita.ac.in>
2018-05-28 17:01:28 +0530

    text to speech added in main activity

--- app/src/main/java/xhedra/test2/MainActivity.java
+++ app/src/main/java/xhedra/test2/MainActivity.java
@@ -3,45 +3,69 @@ package xhedra.test2;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
 
     Button loc_en;
     Button loc_hi;
     Button loc_mr;
+    private TextView eng,hin,marathi;
+    private TextToSpeech engine;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eng = (TextView)findViewById(R.id.eng);
+        hin = (TextView)findViewById(R.id.hin);
+        marathi = (TextView)findViewById(R.id.marathi);
+
+        engine = new TextToSpeech(this,this);
+
         loc_mr = findViewById(R.id.mr);
         loc_hi = findViewById(R.id.hi);
         loc_en = findViewById(R.id.en);
         final String loc = Locale.getDefault().toString();
 
         loc_en.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
+                String english = eng.getText().toString();
+                speak(english); // text to speech of the text view
                 setLocale(loc);
             }
         });
 
         loc_hi.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
+                String hindi = hin.getText().toString();
+                speak(hindi);
                 setLocale("hi");
             }
         });
 
         loc_mr.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
             @Override
             public void onClick(View v) {
+                String marath = marathi.getText().toString();
+                speak(marath);
                 setLocale("mr");
             }
         });
@@ -59,5 +83,19 @@ public class MainActivity extends AppCompatActivity {
         finish();
 
 
+    }
+
+    @Override
+    public void onInit(int i) {
+
+        if(i == TextToSpeech.SUCCESS){
+            engine.setLanguage(Locale.getDefault());
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speak(String s){
+        engine.speak(s,TextToSpeech.QUEUE_FLUSH,null,null);
     }
 }
